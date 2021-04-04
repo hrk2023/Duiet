be818e782ff2d3eee27033caa788d8e72739b43b
hrk2023 <hrkdasgupta025@gmail.com>
2021-04-05 01:47:22 +0530

    Modified config

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from os import path,environ
 
 BASE_DIR = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(BASE_DIR,'.env'))
+# load_dotenv(path.join(BASE_DIR,'.env'))
 class Config:
     SECRET_KEY = environ.get("SECRET_KEY")
     TESTING = False
